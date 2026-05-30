ff6bfd42a266a1c26a44c1fbd448ea0b35ccee1c
a-anzil <289273535+a-anzil@users.noreply.github.com>
2026-05-30 20:27:19 +0200

    Update deploy

--- src/shared/http.js
+++ src/shared/http.js
@@ -1,6 +1,6 @@
 export const BASE_URL = process.env.NODE_ENV === "development"
     ? "http://localhost:8787"
-    : "https://schedule.antonio32a.workers.dev";
+    : "https://e-raspored-api.anzil.dev";
 
 export const getKey = () => localStorage.getItem("key")
 
